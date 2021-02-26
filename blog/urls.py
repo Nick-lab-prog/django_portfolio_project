8045e0eeca71be15c088e4786dc26eecc47728db
Nicola <sin-95@hotmail.it>
2021-02-26 18:21:54 +0100

    modifiche

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'), 
-    
+    #path('<int:blog_id/>', views.detail, name='detail'), 
+    path('home/', views.home, name="home"),
+    path('blog', views.all_blogs, name="blog")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from .models import Blog 
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
 # Create your views here.
+
+app_name = 'blog'
+
 def all_blogs(request):
+	blog_count = Blog.objects.count()
 	blogs = Blog.objects.all() 
 	#per mettere in ordine si mette invece di all order_by('-date')
 	#e per limitare il numero di oggetti mostrati invece si usa una sorta di slice delle liste -> [:x]
 	return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+def detail(request, blog_id):
+	blog = get_object_or_404(Blog, pk=blog_id)#pk sta per primary key
+	return render(request, 'blog/detail.html', {'blog':blog})
 def home(request):
-	return render(request, 'home.html', name="home")
+	return render(request, 'portfolio/home.html')
